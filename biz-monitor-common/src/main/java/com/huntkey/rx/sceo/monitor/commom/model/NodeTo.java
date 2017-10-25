def072ebc2be04c958af6301174d970d7796c34c
unknown <fangkun@hkhf.hkgp.net>
2017-10-25 09:00:46 +0800

    新增属性字段

--- biz-monitor-common/src/main/java/com/huntkey/rx/sceo/monitor/commom/model/NodeTo.java
+++ biz-monitor-common/src/main/java/com/huntkey/rx/sceo/monitor/commom/model/NodeTo.java
@@ -30,7 +30,9 @@ import com.huntkey.rx.sceo.monitor.commom.enums.ChangeType;
 public class NodeTo implements Serializable{
     
     private static final long serialVersionUID = 1L;
-
+    
+    private String tempId;
+    
     private String nodeNo;
     
     private String nodeName;
@@ -39,7 +41,35 @@ public class NodeTo implements Serializable{
     
     private String major;
     
-    private String assit;
+    private String majorText;
+    
+    public String getTempId() {
+		return tempId;
+	}
+
+	public void setTempId(String tempId) {
+		this.tempId = tempId;
+	}
+
+	public String getMajorText() {
+		return majorText;
+	}
+
+	public void setMajorText(String majorText) {
+		this.majorText = majorText;
+	}
+
+	public String getAssitText() {
+		return assitText;
+	}
+
+	public void setAssitText(String assitText) {
+		this.assitText = assitText;
+	}
+
+	private String assit;
+    
+    private String assitText;
     
     private String begin;
     
