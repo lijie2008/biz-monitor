0ed218f9895b85908eadea7aacc1c74eab508f2a
fangkun <fangkun@huntkey.net>
2017-10-27 09:16:29 +0800

    修改入参

--- biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/controller/MonitorController.java
+++ biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/controller/MonitorController.java
@@ -36,7 +36,7 @@ public class MonitorController {
     @RequestMapping(value = "/checkOrder")
     public Result checkOrder(@RequestParam(value = "classId") @NotBlank(message="监管类ID不能为空") String classId,
                              @RequestParam(value = "rootId",defaultValue="") String rootId,
-                             @RequestParam(value = "type") @Range(min=1,max=2) @NotBlank(message="操作类型不能为空") int type){
+                             @RequestParam(value = "type") @Range(min=1,max=2)  int type){
         Result result = new Result();
         result.setRetCode(Result.RECODE_SUCCESS);
         result.setData(service.checkOrder(classId, rootId, type));
