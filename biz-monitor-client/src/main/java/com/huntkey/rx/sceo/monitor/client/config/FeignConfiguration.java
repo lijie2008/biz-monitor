f0a4fc770de28b32dafa74c11e92f5a639d11c77
lijie <confucians2012@163.com>
2017-12-08 15:26:05 +0800

    feigin时间调整

--- biz-monitor-client/src/main/java/com/huntkey/rx/sceo/monitor/client/config/FeignConfiguration.java
+++ biz-monitor-client/src/main/java/com/huntkey/rx/sceo/monitor/client/config/FeignConfiguration.java
@@ -1,9 +1,9 @@
 package com.huntkey.rx.sceo.monitor.client.config;
 
-import feign.Request;
-import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Retryer;
 
 /**
  * Created by zhaomj on 2017/4/25.
@@ -14,10 +14,5 @@ public class FeignConfiguration {
     @Bean
     Retryer retryer(){
         return Retryer.NEVER_RETRY;
-    }
-
-    @Bean
-    Request.Options feignOptions() {
-        return new Request.Options(/**connectTimeoutMillis**/1 * 1000, /** readTimeoutMillis **/100 * 1000);
     }
 }
--- biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/config/FeignConfiguration.java
+++ biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/config/FeignConfiguration.java
@@ -1,9 +1,9 @@
 package com.huntkey.rx.sceo.monitor.provider.config;
 
-import feign.Request;
-import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Retryer;
 
 /**
  * Created by xuyf on 2017/7/27.
@@ -14,10 +14,5 @@ public class FeignConfiguration {
     @Bean
     Retryer retryer(){
         return Retryer.NEVER_RETRY;
-    }
-
-    @Bean
-    Request.Options feignOptions() {
-        return new Request.Options(/**connectTimeoutMillis**/1 * 1000, /** readTimeoutMillis **/100 * 1000);
     }
 }
