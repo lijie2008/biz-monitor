b300908a5e084d911005eb6916ebeb95f569fdca
lijie <confucians2012@163.com>
2017-12-20 15:44:54 +0800

    1111

--- biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/controller/MonitorTreeOrderController.java
+++ biz-monitor-provider/src/main/java/com/huntkey/rx/sceo/monitor/provider/controller/MonitorTreeOrderController.java
@@ -18,7 +18,6 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
